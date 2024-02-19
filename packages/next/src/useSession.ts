56ff496ba6ffb4602ff078d228b1bdb05f295ce2
An Hoang <xuanan2001@pm.me>
2024-02-19 09:45:37 +0700

    init

---
+++ packages/next/src/useSession.ts
@@ -0,0 +1,54 @@
+import LiveState from "phx-live-state";
+import { useMemo, useState, useEffect } from "react";
+
+export type Request<T extends any> = {
+  id: string;
+  resp_body_chunks: string[];
+  req_metadata: T;
+  status: string;
+  resp_code: null | number;
+};
+
+export const useSession = <Metadata extends any>(sessionId?: string | null) => {
+  const [state, setState] = useState<
+    | undefined
+    | {
+        session: { id: string; requests: Request<Metadata>[] };
+        active_requests: Record<string, Request<Metadata>>;
+      }
+  >();
+  const [connected, setConnected] = useState(false);
+
+  const liveState = useMemo(
+    () =>
+      sessionId
+        ? new LiveState({
+            url: "ws://localhost:4000/socket",
+            // url: process.env.NEXT_PUBLIC_USAGE_WS,
+            topic: `session:${sessionId}`,
+            params: {
+              // token: "1234",
+              key: process.env.NEXT_PUBLIC_USAGE_PUBLIC_SHAREABLE,
+            },
+          })
+        : null,
+    [sessionId],
+  );
+
+  useEffect(() => {
+    if (!liveState) return;
+
+    liveState.connect();
+    let handler = () => {
+      setState(liveState.state);
+      setConnected(liveState.connected);
+    };
+    liveState.addEventListener("livestate-change", handler);
+
+    return () => {
+      liveState.removeEventListener("livestate-change", handler);
+    };
+  }, [liveState]);
+
+  return { connected, state };
+};
